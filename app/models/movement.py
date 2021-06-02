621ece3efb28adbc611851979dedd6f14498b0b4
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-02 18:52:27 +0300

    fix: changed money movement table name

--- app/models/movement.py
+++ app/models/movement.py
@@ -5,7 +5,7 @@ from app import db
 
 class MoneyMovementModel(db.Model):
 
-    __tablename__ = 'money-movement'
+    __tablename__ = 'money_movement'
 
     movement_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     modified_date = db.Column(db.Date, default=datetime.datetime.today)
@@ -17,3 +17,4 @@ class MoneyMovementModel(db.Model):
     def repr(self):
         return f'Money movement #{self.movement_id} from {self.originator_person} to {self.receiver_person} ' \
                f'with amount of {self.amount} made on {self.modified_date}'
+
