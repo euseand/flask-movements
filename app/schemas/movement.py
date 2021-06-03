5aefd8b33b15d9f3b3650ffc3f21f92cb3b649a2
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-03 13:30:05 +0300

    fix: fixed movement schema

--- app/schemas/movement.py
+++ app/schemas/movement.py
@@ -3,8 +3,8 @@ from marshmallow import fields, Schema
 
 class MoneyMovementSchema(Schema):
 
-    movementId = fields.Number(attribute='movement_id')
-    modifiedDate = fields.Date(attribute='modified_date')
+    movementId = fields.Integer(attribute='movement_id', dump_only=True)
+    modifiedDate = fields.Date(attribute='modified_date', dump_only=True)
     amount = fields.String(attribute='amount')
     originatorPerson = fields.String(attribute='originator_person')
     receiverPerson = fields.String(attribute='receiver_person')
