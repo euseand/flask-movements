a6401f80aee7ee232b482702dfba6bb62625585f
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-03 15:30:43 +0300

    feat: added uuid support

--- app/models/movement.py
+++ app/models/movement.py
@@ -1,18 +1,21 @@
+import uuid
 import datetime
+
+from sqlalchemy.dialects.postgresql import UUID
 
 from app import db
 
 
 class MoneyMovementModel(db.Model):
-
+    """Money Movement model class"""
     __tablename__ = 'money_movement'
 
-    movement_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movement_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     modified_date = db.Column(db.Date, default=datetime.datetime.today)
-    amount = db.Column(db.String(50))
-    originator_person = db.Column(db.String(100))
-    receiver_person = db.Column(db.String(100))
-    note = db.Column(db.String(100), nullable=True)
+    amount = db.Column(db.String(length=50))
+    originator_person = db.Column(db.String(length=100))
+    receiver_person = db.Column(db.String(length=100))
+    note = db.Column(db.String(length=100), nullable=True)
 
     def repr(self):
         return f'Money movement #{self.movement_id} from {self.originator_person} to {self.receiver_person} ' \
