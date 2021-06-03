9ea7fa2159c341ad45688a89e7429b5c721ee754
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-03 15:31:45 +0300

    fix: fixed schema definition

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,14 @@ db = SQLAlchemy()
 def create_app(config_file_name):
     app = Flask(__name__)
     app.config.from_object(config_file_name)
+
     api = Api(app)
 
     db.init_app(app)
-
     migrate = Migrate(app, db)
 
     with app.app_context():
-        from app.views.movement import movement_blp
-        api.register_blueprint(movement_blp)
+        from app.views.movement import blp
+        api.register_blueprint(blp)
 
         return app
--- app/schemas/movement.py
+++ app/schemas/movement.py
@@ -3,12 +3,13 @@ from marshmallow import fields, Schema
 
 class MoneyMovementSchema(Schema):
 
-    movementId = fields.Integer(attribute='movement_id', dump_only=True)
+    movementId = fields.UUID(attribute='movement_id', dump_only=True)
     modifiedDate = fields.Date(attribute='modified_date', dump_only=True)
-    amount = fields.String(attribute='amount')
-    originatorPerson = fields.String(attribute='originator_person')
-    receiverPerson = fields.String(attribute='receiver_person')
+    amount = fields.String(attribute='amount', required=True)
+    originatorPerson = fields.String(attribute='originator_person', required=True)
+    receiverPerson = fields.String(attribute='receiver_person', required=True)
     note = fields.String(attribute='note')
 
     class Meta:
+        ordered = True
         fields = ('movementId', 'modifiedDate', 'amount', 'originatorPerson', 'receiverPerson', 'note')
