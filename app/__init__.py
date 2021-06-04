9d4e024e804dd6f197a00f95a787dfcefe94c5b8
YevgenySemak <48668000+euseand@users.noreply.github.com>
2021-06-04 14:55:50 +0300

    feat: added common db, migrate and api instances

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,34 @@
 from flask import Flask
-from flask_smorest import Api
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 
-db = SQLAlchemy()
-
-
-def create_app(config_file_name):
+def create_app(config_filename):
     app = Flask(__name__)
-    app.config.from_object(config_file_name)
-
-    api = Api(app)
-
-    db.init_app(app)
-    migrate = Migrate(app, db)
+    app.config.from_object(config_filename)
 
     with app.app_context():
-        from app.views.movements import movement_blp, movements_blp
+
+        from app.common import api, db, migrate
+        api.init_app(app)
+        db.init_app(app)
+        migrate.init_app(app, db)
+
+        #from app.movements.model import db as movements_db
+        #from app.movements.model import migrate as movements_migrate
+        from app.movements.model import MoneyMovementModel
+        from app.movements.controller import movement_blp, movements_blp
+
+        #movements_migrate.init_app(app)
         api.register_blueprint(movement_blp)
         api.register_blueprint(movements_blp)
 
-        return app
+
+        #from app.users.model import db as users_db
+        #from app.users.model import migrate as users_migrate
+        from app.users.model import UserModel
+        from app.users.controller import user_blp, users_blp
+
+        #users_migrate.init_app(app)
+        api.register_blueprint(user_blp)
+        api.register_blueprint(users_blp)
+
+    return app
